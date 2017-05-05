7b452ec5c310501b6ea4786601f6c5a5ee30abaa
oriash93 <oriash93@gmail.com>
2017-05-06 01:46:08 +0300

    Authentication - Added register routes and register page

--- app.js
+++ app.js
@@ -138,3 +138,21 @@ app.post("/campgrounds/:id/comments", function (req, res) {
         }
     });
 });
+
+// Auth Routes - Register
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/campgrounds");
+        });
+    });
+});
