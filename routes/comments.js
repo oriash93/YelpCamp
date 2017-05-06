d07939c8798a3b65e5083bd0968eb70a2c5881cd
oriash93 <oriash93@gmail.com>
2017-05-06 05:32:57 +0300

    Added comment destroy route

--- routes/comments.js
+++ routes/comments.js
@@ -73,6 +73,18 @@ router.put("/:comment_id", function (req, res) {
     });
 });
 
+// Destroy route
+router.delete("/:comment_id", function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 // Middlewares
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
